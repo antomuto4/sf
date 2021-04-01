7d46e34352425aa94f5166c3bb1169c263b73783
アントムト <56132390+antomuto4@users.noreply.github.com>
2021-04-01 13:30:00 +0200

    Update sf.sh

--- sf.sh
+++ sf.sh
@@ -1,10 +1,13 @@
 distro=$(grep -m1 "NAME=" /etc/os-release | sed 's:.*=::')
-ascii=$(<~/ascii/pika) #ascii/yourAsciiArt
+ascii=$(<~/ascii/robot) #ascii/yourAsciiArt
 echo "$ascii"
+echo
 echo -e " \e[4m\e[1m\e[95m$USER\e[0m"@"\e[4m\e[1m\e[95m$HOSTNAME\e[0m "
 echo -e " \e[4m\e[95mOS\e[0m": $distro $(uname -m)
 echo -e " \e[4m\e[95mKernel\e[0m": $(uname -r)
 echo -e " \e[4m\e[95mUptime\e[0m": $(uptime -p | sed 's:.*up::')
 #echo -e "\e[4m\e[95mDE/WM\e[0m": $XDG_CURRENT_DESKTOP
 echo -e " \e[4m\e[95mWM\e[0m": awesome
-echo && echo
+echo
+echo -e " \e[4m\e[95m¥[*.*]¥\e[0m": "And can you offer me proof of your existence? How can you, when neither modern science nor philosophy can explain what life is?"
+echo
